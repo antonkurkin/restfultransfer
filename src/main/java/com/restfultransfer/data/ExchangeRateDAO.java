98f46480920529cecf3400a66ee47c75994c0672
Anton Kurkin <kurkin.anton@gmail.com>
2018-07-27 00:42:43 +0300

    add create exchange rate function to servlet

--- src/main/java/com/restfultransfer/data/ExchangeRateDAO.java
+++ src/main/java/com/restfultransfer/data/ExchangeRateDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.commons.dbutils.DbUtils;
 
@@ -61,6 +62,28 @@ public class ExchangeRateDAO extends H2Connector {
 			return objects;
 		} catch (SQLException e) {
 			throw new Exception("Can't get objects from table ExchangeRates", e);
+		} finally {
+			DbUtils.closeQuietly(connection, sqlStatement, result);
+		}
+	}
+
+	public ExchangeRate Create(Currency currencyFrom, Currency currencyTo, double rate) throws Exception {
+		Connection connection = null;
+		PreparedStatement sqlStatement = null;
+		ResultSet result = null;
+		try {
+			connection = getConnection();
+			sqlStatement = connection.prepareStatement("INSERT INTO ExchangeRates (CurrencyFrom, CurrencyTo, Rate) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			sqlStatement.setString(1, currencyFrom.getCurrencyCode());
+			sqlStatement.setString(2, currencyTo.getCurrencyCode());
+			sqlStatement.setDouble(3, rate);
+			int rowCount = sqlStatement.executeUpdate();
+			if (rowCount == 0)
+				throw new Exception("Exchange rate wasn't created in DB");
+			result = sqlStatement.getGeneratedKeys();
+			return Get(currencyFrom, currencyTo);
+		} catch (SQLException e) {
+			throw new Exception("Can't create exchange rate in DB", e);
 		} finally {
 			DbUtils.closeQuietly(connection, sqlStatement, result);
 		}
--- src/main/java/com/restfultransfer/servlet/ExchangeRateServlet.java
+++ src/main/java/com/restfultransfer/servlet/ExchangeRateServlet.java
@@ -4,6 +4,7 @@ import java.util.Currency;
 import java.util.Vector;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,8 +17,8 @@ import com.restfultransfer.data.ExchangeRateDAO;
 @Produces(MediaType.APPLICATION_JSON)
 public class ExchangeRateServlet {
     @GET
-    @Path("/{CurrencyFrom}/{CurrencyTo}")
-    public ExchangeRate Get(@PathParam("CurrencyFrom") String currencyFrom, @PathParam("CurrencyTo") String currencyTo) throws Exception {
+    @Path("/{currencyFrom}/{currencyTo}")
+    public ExchangeRate Get(@PathParam("currencyFrom") String currencyFrom, @PathParam("currencyTo") String currencyTo) throws Exception {
     	return (new ExchangeRateDAO()).Get(Currency.getInstance(currencyFrom), Currency.getInstance(currencyTo));
     }
     
@@ -25,5 +26,11 @@ public class ExchangeRateServlet {
     @Path("/list")
     public Vector<ExchangeRate> GetAll() throws Exception {
     	return (new ExchangeRateDAO()).GetAll();
+    }
+
+    @POST
+    @Path("/new/{currencyFrom},{currencyTo},{rate}")
+    public ExchangeRate Create(@PathParam("currencyFrom") String currencyFrom, @PathParam("currencyTo") String currencyTo, @PathParam("rate") Double rate) throws Exception {
+    	return (new ExchangeRateDAO()).Create(Currency.getInstance(currencyFrom), Currency.getInstance(currencyTo), rate);
     }
 }
