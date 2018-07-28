0d3930b75db9a9fc733b90a15f71aa409b9746f3
Anton Kurkin <kurkin.anton@gmail.com>
2018-07-28 21:57:15 +0300

    add rest of tests for Client

--- src/test/java/com/restfultransfer/DBTests.java
+++ src/test/java/com/restfultransfer/DBTests.java
@@ -69,16 +69,8 @@ public class DBTests {
 			assertEquals("alex", client.Name());
 			assertEquals(true, client.isActive());
 			assertNotEquals(new Timestamp(0), client.Created());
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("SQL exception");
-		}
-	}
 
-	@Test
-	public void GetClientWrong() {
-		try {
-			Client client = clientDAO.Get(99);
+			client = clientDAO.Get(99);
 			assertEquals(null, client);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -95,6 +87,9 @@ public class DBTests {
 			assertEquals(1, changed);
 			client = clientDAO.Get(client.Id());
 			assertEquals("dmitry", client.Name());
+
+			changed = clientDAO.ChangeName(99, "dmitry");
+			assertEquals(0, changed);
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("SQL exception");
@@ -102,10 +97,55 @@ public class DBTests {
 	}
 
 	@Test
-	public void ChangeClientNameWrong() {
+	public void GetClientByName() {
 		try {
-			int changed = clientDAO.ChangeName(99, "dmitry");
+			Vector<Client> clients = clientDAO.GetAllByName("dmitry");
+			assertEquals(0, clients.size());
+			
+			clients = clientDAO.GetAllByName("anton");
+			assertEquals(1, clients.size());
+			clientDAO.ChangeName(1, "alex");
+			clients = clientDAO.GetAllByName("alex");
+			assertEquals(2, clients.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("SQL exception");
+		}
+	}
+
+	@Test
+	public void ActivateClient() {
+		try {
+			Client client = clientDAO.Get(1);
+			assertEquals(true, client.isActive());
+
+			int changed = clientDAO.SetActive(client.Id(), false);
+			assertEquals(1, changed);
+			client = clientDAO.Get(client.Id());
+			assertEquals(false, client.isActive());
+
+			changed = clientDAO.SetActive(client.Id(), true);
+			assertEquals(1, changed);
+			client = clientDAO.Get(client.Id());
+			assertEquals(true, client.isActive());
+
+			changed = clientDAO.SetActive(99, true);
 			assertEquals(0, changed);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("SQL exception");
+		}
+	}
+
+	@Test
+	public void CreateClient() {
+		try {
+			Client clientNew = clientDAO.Create("dmitry");
+			assertEquals("dmitry", clientNew.Name());
+			
+			Client clientGet = clientDAO.Get(clientNew.Id());
+			assertEquals(clientNew.Name(), clientGet.Name());
+			assertEquals(clientNew.Created(), clientGet.Created());
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail("SQL exception");
