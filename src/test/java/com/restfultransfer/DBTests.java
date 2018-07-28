7ec2628cd46840b4533151cf7544963ab76b0a13
Anton Kurkin <kurkin.anton@gmail.com>
2018-07-28 21:10:08 +0300

    update test

--- src/test/java/com/restfultransfer/DBTests.java
+++ src/test/java/com/restfultransfer/DBTests.java
@@ -1,24 +1,34 @@
 package com.restfultransfer;
 
 import com.restfultransfer.data.*;
-import com.restfultransfer.data.H2Connector;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Vector;
 
 import org.junit.Test;
 import org.junit.Before;
-import org.junit.Assert;
+import org.junit.BeforeClass;
 
 public class DBTests {
-	
-	@Before
-	public void Init() {
+
+	@BeforeClass
+	public static void InitDatabase() {
     	try {
 			H2Connector.LoadSQLFile("src/main/database.sql");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Database loading failed");
+		}
+	}
+	
+	@Before
+	public void InitDataset() {
+    	try {
 	    	H2Connector.LoadSQLFile("src/test/dataset.sql");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			fail("Dataset loading failed");
 		}
 	}
 	
@@ -26,23 +36,22 @@ public class DBTests {
 	public void TestInitDataset() {
 		try {
 			Vector<Client> clients = (new ClientDAO()).GetAll();
-			Assert.assertEquals(4, clients.size());
+			assertEquals(4, clients.size());
 			for (int i = 0; i < clients.size(); i++)
-				Assert.assertEquals(i+1, clients.get(i).Id());
+				assertEquals(i+1, clients.get(i).Id());
 			Vector<Account> accounts = (new AccountDAO()).GetAll();
-			Assert.assertEquals(8, accounts.size());
+			assertEquals(8, accounts.size());
 			for (int i = 0; i < accounts.size(); i++)
-				Assert.assertEquals(i+1, accounts.get(i).Id());
+				assertEquals(i+1, accounts.get(i).Id());
 			Vector<Transaction> transactions = (new TransactionDAO()).GetAll();
-			Assert.assertEquals(9, transactions.size());
+			assertEquals(9, transactions.size());
 			for (int i = 0; i < transactions.size(); i++)
-				Assert.assertEquals(i+1, transactions.get(i).Id());
+				assertEquals(i+1, transactions.get(i).Id());
 			Vector<ExchangeRate> exchangeRates = (new ExchangeRateDAO()).GetAll();
-			Assert.assertEquals(6, exchangeRates.size());
+			assertEquals(6, exchangeRates.size());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+			fail("SQL exception");
 		}
 	}
 }
